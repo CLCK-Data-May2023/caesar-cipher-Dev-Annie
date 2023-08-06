5f9e3bc6d6ddc6bcb50b2359bdd5bf6d989de82c
Dev-Annie <85464723+Dev-Annie@users.noreply.github.com>
2023-08-06 07:41:27 -0400

    added cipher

--- cipher.py
+++ cipher.py
@@ -1,1 +1,7 @@
-testing here
+phrase = input('Type in your secret message here\n')
+
+encryption = phrase.maketrans("abcdefghijklmnopqrstuvwxyz", "efghijklmnopqrstuvwxyzabcd")
+output = phrase.translate(encryption)
+print("Your message : ", phrase)
+print("Your secret message becomes: ", output)
+
