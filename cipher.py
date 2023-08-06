20ba2a87560af690605eb7d4e9cc158c471173ea
Dev-Annie <85464723+Dev-Annie@users.noreply.github.com>
2023-08-06 08:23:03 -0400

    recognize capital letters

--- cipher.py
+++ cipher.py
@@ -3,8 +3,8 @@
 
 phrase = input('Please enter a sentence\n')
 
-encryption = phrase.maketrans("abcdefghijklmnopqrstuvwxyz", "fghijklmnopqrstuvwxyzabcde")
+encryption = phrase.maketrans("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", "fghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcde")
 output = phrase.translate(encryption)
-print("The encrypted sentence is: ", output)
+print("The encrypted sentence is: ",output)
 
 #sys.exit("Congratulations on making a secret message. Goodbye")
