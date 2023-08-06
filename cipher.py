5fc36cbab2b2898d1934f6113c961dd460915bd9
Dev-Annie <85464723+Dev-Annie@users.noreply.github.com>
2023-08-06 08:02:29 -0400

    changed shift to 5

--- cipher.py
+++ cipher.py
@@ -1,7 +1,11 @@
+import sys
+print("Welcome to the Ceasar Cipher!")
+
 phrase = input('Type in your secret message here\n')
 
-encryption = phrase.maketrans("abcdefghijklmnopqrstuvwxyz", "efghijklmnopqrstuvwxyzabcd")
+encryption = phrase.maketrans("abcdefghijklmnopqrstuvwxyz", "fghijklmnopqrstuvwxyzabcde")
 output = phrase.translate(encryption)
 print("Your message : ", phrase)
 print("Your secret message becomes: ", output)
 
+sys.exit("Congratulations on making a secret message. Goodbye")
