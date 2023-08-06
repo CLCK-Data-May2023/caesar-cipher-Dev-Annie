02957a4eee4e897c61edaf692c4011159334e924
Dev-Annie <85464723+Dev-Annie@users.noreply.github.com>
2023-08-06 08:25:00 -0400

    space removed

--- cipher.py
+++ cipher.py
@@ -5,6 +5,6 @@ phrase = input('Please enter a sentence\n')
 
 encryption = phrase.maketrans("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", "fghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcde")
 output = phrase.translate(encryption)
-print("The encrypted sentence is: ",output)
+print("The encrypted sentence is:",output)
 
 #sys.exit("Congratulations on making a secret message. Goodbye")
